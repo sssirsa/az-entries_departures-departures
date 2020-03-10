b43d6ceef189e4426b9b3973b6d3f6c356c89c4c
Platae <platae93@gmail.com>
2020-03-10 16:27:19 -0600

    Se elimin{o el sufijo "_id" de los campos relacionados a las salidas
    -cabinets
    -sucursal_origen
    -sucursal_destino
    -udn_origen
    -udn_destino
    -proveedor_destino

--- warehouse-departures/index.js
+++ warehouse-departures/index.js
@@ -151,12 +151,12 @@ module.exports = function (context, req) {
         //TODO: Get person data trough userid and save it in the entry data
         let departure; //Base object
         var userId = null;
-        var destinationAgencyId = req.body['udn_destino_id'];
-        var destinationSubsidiaryId = req.body['sucursal_destino_id'];
-        var originAgencyId = req.body['udn_origen_id'];
-        var originSubsidiaryId = req.body['sucursal_origen_id'];
-        var transportDriverId = req.body['operador_transporte_id'];
-        var transportKindId = req.body['tipo_transporte_id']; //Non mandatory
+        var destinationAgencyId = req.body['udn_destino'];
+        var destinationSubsidiaryId = req.body['sucursal_destino'];
+        var originAgencyId = req.body['udn_origen'];
+        var originSubsidiaryId = req.body['sucursal_origen'];
+        var transportDriverId = req.body['operador_transporte'];
+        var transportKindId = req.body['tipo_transporte']; //Non mandatory
 
         validate();
 
@@ -180,7 +180,7 @@ module.exports = function (context, req) {
             if (transportKindId) {
                 transportKind = await searchTransportKind(transportKindId);
             }
-            let fridges = await searchAllFridges(req.body['cabinets_id']);
+            let fridges = await searchAllFridges(req.body['cabinets']);
 
             let precedentPromises = [originAgency, originSubsidiary, destinationAgency, destinationSubsidiary, transportDriver, transportKind, fridges];
 
@@ -204,7 +204,7 @@ module.exports = function (context, req) {
                         cabinets: fridges
                     };
 
-                    //await deleteAllControl(req.body['cabinets_id']);
+                    //await deleteAllControl(req.body['cabinets']);
                     await updateFridges(fridges);
 
                     let response = await writeDeparture();
@@ -303,7 +303,7 @@ module.exports = function (context, req) {
             }
 
             //Fridge array validation
-            if (!req.body.cabinets_id) {
+            if (!req.body.cabinets) {
                 //No array
                 context.res = {
                     status: 400,
@@ -316,7 +316,7 @@ module.exports = function (context, req) {
                 };
                 context.done();
             }
-            if (req.body.cabinets_id.length === 0) {
+            if (req.body.cabinets.length === 0) {
                 //Empty array
                 context.res = {
                     status: 400,
@@ -839,6 +839,7 @@ module.exports = function (context, req) {
             let newValues = {
                 sucursal: null,
                 udn: null,
+                nuevo: false,
                 estatus_unilever: unlieverStatus,
                 fecha_ingreso: null
             };
